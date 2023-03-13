d37e95a6f9157bf2a89e686426f230fe03e88576
almoataz97 <126671083+almoataz97@users.noreply.github.com>
2023-03-13 15:54:15 +0100

    Add files via upload

---
+++ LAB3_PIR_sensor.ino
@@ -0,0 +1,28 @@
+int ledPin = LED_BUILTIN;
+int inputPin = D1;
+int pirState = LOW;
+int val = 0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledPin, OUTPUT);
+pinMode(inputPin, INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+val = digitalRead(inputPin);
+if (val == HIGH){
+  digitalWrite(ledPin, HIGH);
+  if (pirState == LOW){
+    Serial.println("Motion detected!");
+    pirState = HIGH;
+  }
+} else {
+  digitalWrite(ledPin, LOW);
+  if (pirState == HIGH){
+    Serial.println("Motion ended!");
+    pirState = LOW;
+  }
+}
+}
